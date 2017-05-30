177bc8f9fc075d59439768a98e9ff90af2b51097
Ignacio Mieites <ia.mieites@gmail.com>
2017-05-30 02:35:46 -0300

    commit inicial, node api con restify

---
+++ routes/index.js
@@ -0,0 +1,124 @@
+'use strict'
+
+/**
+ * Module Dependencies
+ */
+const _ = require('lodash'),
+errors = require('restify-errors')
+
+/**
+ * Model Schema
+ */
+const Todo = require('../models/todo')
+
+/**
+ * POST
+ */
+server.post('/todos', function(req, res, next) {
+
+    let data = req.body || {}
+    console.log(data);
+
+    let todo = new Todo(data)
+    todo.save(function(err) {
+        if (err) {
+            log.error(err)
+            return next(new errors.InternalError(err.message))
+            next()
+        }
+        res.send(201)
+        log.info(
+            'POST /todos |',req
+        )
+        next()
+    })
+})
+
+/**
+ * LIST
+ */
+server.get('/todos', function(req, res, next) {
+
+    Todo.apiQuery(req.params, function(err, docs) {
+        if (err) {
+            log.error(err)
+            return next(new errors.InvalidContentError(err.errors.name.message))
+        }
+        res.send(docs)
+        log.info(
+            'GET /todos'
+        )
+        next()
+    })
+})
+
+/**
+ * GET
+ */
+server.get('/todos/:todo_id', function(req, res, next) {
+    Todo.findOne({ _id: req.params.todo_id }, function(err, doc) {
+        if (err) {
+            log.error(err)
+            return next(new errors.InvalidContentError(err.errors.name.message))
+        }
+        res.send(doc)
+        next()
+    })
+})
+
+/**
+ * UPDATE
+ */
+server.put('/todos/:todo_id', function(req, res, next) {
+
+    let data = req.body || {}
+
+    if (!data._id) {
+		_.extend(data, {
+			_id: req.params.todo_id
+		})
+	}
+
+    Todo.findOne({ _id: req.params.todo_id }, function(err, doc) {
+
+		if (err) {
+			log.error(err)
+			return next(new errors.InvalidContentError(err.errors.name.message))
+		} else if (!doc) {
+			return next(new errors.ResourceNotFoundError('The resource you requested could not be found.'))
+		}
+
+		Todo.update({ _id: data._id }, data,{ runValidators: true }, function(err) {
+
+			if (err) {
+        if (err.name == 'ValidationError') {
+          res.send(400, {"error":err.name})
+        }
+        else {
+          log.error(err)
+  				return next(new errors.InvalidContentError(err.errors.name.message))
+        }
+			}
+      else { // no errors
+        res.send(200, data)
+              next()
+      }
+		})
+	})
+})
+
+/**
+ * DELETE
+ */
+server.del('/todos/:todo_id', function(req, res, next) {
+
+    Todo.remove({ _id: req.params.todo_id }, function(err) {
+		if (err) {
+			log.error(err)
+			return next(new errors.InvalidContentError(err.errors.name.message))
+		}
+		res.send(204)
+        next()
+	})
+
+})
